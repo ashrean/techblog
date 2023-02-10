3f26de8a7c980aeff0a5ad07928029e736dadee5
Ashley Sese <sese.ashrean@gmail.com>
2023-02-09 23:54:08 -0800

    finished the api's

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.blog('/:id', async(req, res) => {
+    try {
+        const message = await Comment.create({
+            ...req.body,
+            post_id: req.params.id,
+            user_id: req.session.user_id
+        });
+        res.json({message})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,1 +1,67 @@
 const router = require('express').Router();
+const { User, Blog, Comment } = require('../models');
+const auth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+
+        const user = await Blog.findAll({
+            where : {
+                user_id : req.session.user_id
+            }, include : [{
+                model: Comment,
+                include : {
+                    model : User,
+                    attributes : ['user_name']
+                }
+            }]
+        },
+        );
+
+
+        const userPosts = user.map((blog) => post.get({ plain: true}));
+
+            res.render('dashboard', {
+                userPosts,
+                logged_in: req.session.logged_in,
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+
+        const post = await Blog.findByPk(req.params.id,
+           {  include : [{
+                model: Comment,
+                include : {
+                    model : User,
+                    attributes : ['user_name']
+                }
+            },
+        {
+            model: User,
+            attributes: ['user_name']
+        }]
+        },
+        );
+
+
+        const editPost = blog.get({ plain: true });
+
+            res.render('editDeletePost', {
+                editPost,
+
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(400).json(err);
+    }
+});
+
+
+
+module.exports = router;
